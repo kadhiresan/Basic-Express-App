6c2a244937d58ba52c2e4a91cbb9369563eebff4
kadhiresan <kkadhir10@gmail.com>
2016-11-27 19:39:14 +0530

    Added code for form and file uplaod

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
 
 //Allow user to access static files
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(multer({ dest: '/tmp/'}));
 
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
@@ -21,11 +28,42 @@ app.get('/file.html', function (req, res) {
   res.sendFile( __dirname + "/" + "file.html" );
 })
 
-
 // This responds a GET request for abcd, abxcd, ab123cd, and so on
 app.get('/ab*cd', function(req, res) {   
    console.log("Got a GET request for /ab*cd");
    res.send('Page Pattern Match');
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/file_upload', function (req, res) {
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+   
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 app.listen(3000, function () {
